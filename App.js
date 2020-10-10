559c43a7f2d7cfcbf83aef9b49367dd74eb3a184
Abdur Rakib Rony <65207907+ronyfr3@users.noreply.github.com>
2020-10-10 10:36:46 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Responsive from './Responsive'
+
+function App() {
+  return (
+    <div>
+      <Responsive/>
+    </div>
+  )
+}
+
+export default App
---
+++ Responsive.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import './App.css'
+
+function Responsive() {
+    return (
+        <div className="wrapper">
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+         <div className='box'>
+            <p>And the only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle. You have to trust in something – your gut, destiny, life, karma, whatever that somehow the dots will connect in the future.” — Steve Jobs
+            </p>
+           <div className="content">
+                <div className="info">
+                <div className="name">Philips</div>
+                <div className="job">Designer</div>    
+                </div>
+                <div className="image">
+                  <img src="https://th.bing.com/th/id/OIP.bqj9H7XnIR6XAzy3ApBQQAHaLH?w=182&h=273&c=7&o=5&pid=1.7" alt="" />
+                </div>
+           </div>
+         </div>
+        </div>
+    )
+}
+
+export default Responsive
